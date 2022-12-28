424dc4fad01b5bd36734cfd1f8709eebc6c4498e
dineshkarthikr <dineshkarthik.r@gmail.com>
2022-12-28 14:32:52 +0530

    add: Flyweight

---
+++ Structural Patterns/Flyweight/C++/main.cc
@@ -0,0 +1,54 @@
+// The flyweight class contains a portion of the state of a
+// tree. These fields store values that are unique for each
+// particular tree. For instance, you won't find here the tree
+// coordinates. But the texture and colors shared between many
+// trees are here. Since this data is usually BIG, you'd waste a
+// lot of memory by keeping it in each tree object. Instead, we
+// can extract texture, color and other repeating data into a
+// separate object which lots of individual tree objects can
+// reference.
+class TreeType is
+    field name
+    field color
+    field texture
+    constructor TreeType(name, color, texture) { ... }
+    method draw(canvas, x, y) is
+        // 1. Create a bitmap of a given type, color & texture.
+        // 2. Draw the bitmap on the canvas at X and Y coords.
+
+// Flyweight factory decides whether to re-use existing
+// flyweight or to create a new object.
+class TreeFactory is
+    static field treeTypes: collection of tree types
+    static method getTreeType(name, color, texture) is
+        type = treeTypes.find(name, color, texture)
+        if (type == null)
+            type = new TreeType(name, color, texture)
+            treeTypes.add(type)
+        return type
+
+// The contextual object contains the extrinsic part of the tree
+// state. An application can create billions of these since they
+// are pretty small: just two integer coordinates and one
+// reference field.
+class Tree is
+    field x,y
+    field type: TreeType
+    constructor Tree(x, y, type) { ... }
+    method draw(canvas) is
+        type.draw(canvas, this.x, this.y)
+
+// The Tree and the Forest classes are the flyweight's clients.
+// You can merge them if you don't plan to develop the Tree
+// class any further.
+class Forest is
+    field trees: collection of Trees
+
+    method plantTree(x, y, name, color, texture) is
+        type = TreeFactory.getTreeType(name, color, texture)
+        tree = new Tree(x, y, type)
+        trees.add(tree)
+
+    method draw(canvas) is
+        foreach (tree in trees) do
+            tree.draw(canvas)
